e2fc6e2b95d21616ad612ff41820557c5550df4f
atej <taneja.aseem@gmail.com>
2025-08-05 05:50:14 +0530

    format

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+// @ts-check
+/** @type {import('prettier').Config} */
+export default {
+  printWidth: 100,
+  singleQuote: true,
+  semi: false,
+  proseWrap: 'always',
+
+  plugins: ['prettier-plugin-organize-imports'],
+}
