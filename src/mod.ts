8590429f325b64bcb6401a58c2d48f0a8a3dc7a5
atej <taneja.aseem@gmail.com>
2025-08-07 23:36:26 +0530

    do not alias paths

--- src/mod.ts
+++ src/mod.ts
@@ -1,5 +1,5 @@
-export { parseNumber } from '@/utils/parse-number'
+export { parseNumber } from './utils/parse-number'
 
-export type { Equal } from '@/types/equal'
-export type { Expect } from '@/types/expect'
-export type { ParseResult } from '@/types/parse-result'
+export type { Equal } from './types/equal'
+export type { Expect } from './types/expect'
+export type { ParseResult } from './types/parse-result'
