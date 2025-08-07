92bc045c3590a31a67e0360cf00c34e9dcea4b1c
atej <taneja.aseem@gmail.com>
2025-08-07 22:46:42 +0530

    fix

--- src/mod.ts
+++ src/mod.ts
@@ -1,5 +1,5 @@
 export { parseNumber } from './utils/parse/parse-number.ts'
 
-export type { Booleanish } from './utils/types/booleanish.ts'
 export type { Equal } from './utils/types/equal.ts'
 export type { Expect } from './utils/types/expect.ts'
+export type { ParseResult } from './utils/types/parse-result.ts'
