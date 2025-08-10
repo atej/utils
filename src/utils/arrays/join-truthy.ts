9b22bc3a99aac6acf4173a93e8910f2492151a0d
atej <taneja.aseem@gmail.com>
2025-08-11 03:00:18 +0530

    remove remeda dependency

--- src/utils/arrays/join-truthy.ts
+++ src/utils/arrays/join-truthy.ts
@@ -1,17 +1,13 @@
-import { join } from '@remeda/remeda'
-
-type JoinableItem = bigint | boolean | number | string | null | undefined
-
 /**
  * Joins the truthy elements of the array by: casting them to a string and
  * concatenating them one to the other, with the provided glue string in between
  * every two elements.
  *
- * @param strings - The array of strings to join.
- * @param separator - The separator to use between the strings.
+ * @param data - The array of strings to join.
+ * @param glue - The separator to use between the strings.
  * @returns The joined string.
  * @signature
- *    joinTruthy(strings, separator)
+ *    joinTruthy(data, glue)
  * @example
  *    joinTruthy(['Hello', 'World', null], ' ') // => 'Hello World'
  * @dataFirst
@@ -22,5 +18,7 @@ export function joinTruthy<T extends ReadonlyArray<JoinableItem> | [], Glue exte
   glue: Glue,
 ): string {
   const truthyElements = data.filter(Boolean)
-  return join(truthyElements, glue)
+  return truthyElements.join(glue)
 }
+
+type JoinableItem = bigint | boolean | number | string | null | undefined
